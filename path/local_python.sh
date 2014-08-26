86bbfb0ee1db4b468b788a73fdb6ee45b22981a7
Matthew Cox <matt.cox@runkeeper.com>
2014-08-26 10:05:30 -0400

    init pyenv; better pythonpath

--- path/local_python.sh
+++ path/local_python.sh
@@ -1,11 +1,13 @@
 # tell python about our stuff
 if [ "${HOME}/lib/python2.7/site-packages" ]; then
-  export PYTHONPATH="$PYTHONPATH:${HOME}/lib/python2.7/site-packages/"
+  export PYTHONPATH="${HOME}/lib/python2.7/site-packages/:$PYTHONPATH:"
 fi
 
+# look for pyenv - and set it up
 if [ -r "${HOME}/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
 fi
 
 # add pip installed bins to the path
