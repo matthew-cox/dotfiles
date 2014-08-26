5266c6e0799354084ebde51e66b1a2154340f560
Matthew Cox <matt.cox@runkeeper.com>
2014-08-26 09:49:57 -0400

    smarter pyenv? basic git stuff

--- path/local_python.sh
+++ path/local_python.sh
@@ -3,6 +3,11 @@ if [ "${HOME}/lib/python2.7/site-packages" ]; then
   export PYTHONPATH="$PYTHONPATH:${HOME}/lib/python2.7/site-packages/"
 fi
 
+if [ -r "${HOME}/.pyenv" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin"
+fi
+
 # add pip installed bins to the path
 if [ -x "$(which pyenv 2>/dev/null)" ]; then
   PYPATH="$(dirname $(pyenv which python))"
