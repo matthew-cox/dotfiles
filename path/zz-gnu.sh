410ac7037f3339e3882004e5d5627975fc71bbc1
Matthew Cox <mcox@fiftythree.com>
2015-12-10 10:44:15 -0500

    Some fixes. More ssh configs. Add GNU coreutils to front of PATH.

---
+++ path/zz-gnu.sh
@@ -0,0 +1,5 @@
+# prefer gnu tools rather than OS X
+if [ -d "/usr/local/opt/coreutils/libexec/gnubin" ]; then
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+fi
