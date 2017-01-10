bb9776601d8b6ad827cb7be7ffcc9205ef88d729
Matthew Cox <mcox@fiftythree.com>
2017-01-10 10:29:39 -0500

    previously uncaptured stuff

--- path/zz-ruby.sh
+++ path/zz-ruby.sh
@@ -1,6 +1,12 @@
-# ruby / rvm shite
-if [ -d "$HOME/.gem/ruby/1.8/bin" ]; then
-  export PATH="${HOME}/.gem/ruby/1.8/bin:${PATH}"
+# ruby / rbenv / rvm shite
+
+if [ -d "$HOME/.rbenv/bin" ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
 fi
+
+# if [ -d "$HOME/.gem/ruby/1.8/bin" ]; then
+#   export PATH="${HOME}/.gem/ruby/1.8/bin:${PATH}"
+# fi
 
 ###[[ -s "/Users/mcox/.rvm/scripts/rvm" ]] && source "/Users/mcox/.rvm/scripts/rvm"  # This loads RVM into a shell session
