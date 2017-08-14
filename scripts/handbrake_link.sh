8ab39e7efafbf1e4bd060a3ed475f5f3bc718b4b
Matthew Cox <matt.cox@simplisafe.com>
2017-08-14 17:47:21 -0400

    Handbrake links

---
+++ scripts/handbrake_link.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# Make links for Handrake to MKMKV
+#
+set -o errexit -o pipefail -o nounset
+. "$(dirname "$0")/common.sh"
+#
+#
+#
+mkdir -p "${HOME}/lib"
+#
+#
+LIB_SRC="/Applications/MakeMKV.app/Contents/lib/libmmbd.dylib"
+
+LIB_TARGETS=(
+libaacs.dylib
+libbdplus.dylib
+)
+  
+if [[ -f "${LIB_SRC}" ]]; then
+
+  for target in "${LIB_TARGETS[@]}"; do
+    if [[ ! -e "${HOME}/lib/${target}" ]]; then
+
+      putsuccess "Creating link for '${target}'"
+      ln -s "${LIB_SRC}" "${HOME}/lib/${target}"
+
+    elif [[ -L "${HOME}/lib/libaacs.dylib" ]]; then
+
+      link_target=$(stat ${target} --format '%N' | awk '{print $3}' | tr -d "'")
+
+      if [[ "${link_target}" != "${LIB_SRC}" ]]; then
+        puterr "Link '${target}' exists but points to '${link_target}'!"
+        exit 1
+      else
+        putsuccess "Correct link for '${target}' already exists"
+      fi
+    else
+
+      puterr "'${target}' exists but is not a link!"
+      exit 1
+    fi
+  done
+fi
