6af68c987b7743244d4f3eb88d6a97e83a56cac6
Matthew Cox <matt.cox@simplisafe.com>
2017-08-15 15:58:48 -0400

    sudo config for launchctl without password

---
+++ scripts/sudo_config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# allow admin users to disable services without a password
+#
+echo "*** Creating sudo config..."
+sudo sh -c 'echo "
+#
+# allow admin users to disable services without a password
+#
+%admin          ALL = (root) NOPASSWD: /bin/launchctl stop *
+%admin          ALL = (root) NOPASSWD: /usr/bin/pkill -9 keyboardservicesd
+" > /private/etc/sudoers.d/10-service-control'
