e3e48667de43efcf972f61e8c4b02d9ec67de336
Matthew Cox <matt.cox@simplisafe.com>
2017-08-22 16:09:02 -0400

    More launchctl fixes
    Malware app
    
    Additional sudo config

--- scripts/sudo_config.sh
+++ scripts/sudo_config.sh
@@ -7,6 +7,7 @@ sudo sh -c 'echo "
 #
 # allow admin users to disable services without a password
 #
+%admin          ALL = (root) NOPASSWD: /bin/launchctl disable *
 %admin          ALL = (root) NOPASSWD: /bin/launchctl stop *
 %admin          ALL = (root) NOPASSWD: /usr/bin/pkill -9 keyboardservicesd
 " > /private/etc/sudoers.d/10-service-control'
