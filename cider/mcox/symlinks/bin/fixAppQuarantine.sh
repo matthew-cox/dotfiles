940dae4613a32f71096ea6a07df266814f3b7eca
Matthew Cox <matt.cox@simplisafe.com>
2018-10-13 12:58:25 -0400

    fixAppQuarantine will now work on a full path

--- cider/mcox/symlinks/bin/fixAppQuarantine.sh
+++ cider/mcox/symlinks/bin/fixAppQuarantine.sh
@@ -8,6 +8,8 @@ THE_APP_TO_FIX="$1"
 
 if [[ -d "/Applications/${THE_APP_TO_FIX}/" ]]; then
   xattr -rd com.apple.quarantine "/Applications/${THE_APP_TO_FIX}/"
+elif [[ -d "${THE_APP_TO_FIX}" ]]; then
+  xattr -rd com.apple.quarantine "${THE_APP_TO_FIX}"
 else
   echo -e "\a*** Sorry, that app doesn't exist."
 fi
