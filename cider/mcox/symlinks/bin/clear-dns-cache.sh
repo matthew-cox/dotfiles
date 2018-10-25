22d851fadfad336373f0942391bda744ef1cbc3b
Matthew Cox <matt.cox@simplisafe.com>
2018-10-24 20:55:44 -0400

    capture and clean

--- cider/mcox/symlinks/bin/clear-dns-cache.sh
+++ cider/mcox/symlinks/bin/clear-dns-cache.sh
@@ -4,6 +4,9 @@
 #
 if [[ "$(uname -s)" == "Darwin" ]]; then
   echo "Clearing DNS cache..."
+  if command -v discoveryutil; then
+    sudo discoveryutil udnsflushcaches
+  fi
   sudo dscacheutil -flushcache
   sudo killall -HUP mDNSResponder
 fi
