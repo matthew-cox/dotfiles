8fdce3bf24ca47639094196b4d91a1cb4c128fd6
Matthew Cox <matt.cox@simplisafe.com>
2018-10-24 20:58:14 -0400

    shellcheck

--- cider/mcox/symlinks/bin/cleanFluidApp.sh
+++ cider/mcox/symlinks/bin/cleanFluidApp.sh
@@ -5,7 +5,7 @@ set -o nounset
 THE_APP_TO_CLEAN=$1
 
 if [[ -d "${HOME}/Library/Application Support/Fluid/FluidApps/${THE_APP_TO_CLEAN}/" ]]; then
-  cd "${HOME}/Library/Application Support/Fluid/FluidApps/${THE_APP_TO_CLEAN}"
+  cd "${HOME}/Library/Application Support/Fluid/FluidApps/${THE_APP_TO_CLEAN}" || exit
   rm --interactive=once -rf LocalStorage/* cookies/cookies DownloadArchive webhistory
 else
   echo -e "\a*** Sorry, that app doesn't exist."
