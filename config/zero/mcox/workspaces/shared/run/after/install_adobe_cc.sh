279a8431ab3923158535ccf292d61270eb2d913a
Matthew Cox <matt.cox@simplisafe.com>
2021-01-12 17:42:17 -0500

    Cleanup and separation of concerns

---
+++ config/zero/mcox/workspaces/shared/run/after/install_adobe_cc.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# allow admin users to disable services without a password
+#
+# set -x
+WORKDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd -P)
+SCRIPTDIR="${HOME}/.dotfiles/scripts"
+readonly WORKDIR SCRIPTDIR
+#
+##############################################################################
+#
+# Load some utilities
+#
+readonly THE_UTILS=( "common" )
+
+for utility in "${THE_UTILS[@]}"; do
+  if [[ -r "${SCRIPTDIR}/utils_${utility}.sh" ]]; then
+    source "${SCRIPTDIR}/utils_${utility}.sh"
+  fi
+done
+
+_oldopts="$(set +o)"
+
+# save the existing options
+set +o errexit
+putinfo "Searching for Creative Cloud Installer..."
+_installer=$(find /usr/local/Caskroom/adobe-creative-cloud -iname '*Install.app' 2>/dev/null)
+
+if [[ -d "${_installer}" ]]; then
+  putsuccess "done"
+  open "${_installer}"
+else
+  puterr "unable to find installer"
+done
+
+
+# restore the settings
+eval "${_oldopts}"
+
